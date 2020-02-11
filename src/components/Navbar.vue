29e7a6cd73d741364035c9bb2e621925392c6dbe
Manu Castrillon <manu.cm43@gmail.com>
2020-02-10 22:21:27 -0500

    Revert "Develop (#12)"
    This reverts commit c1a43f28796e792b37e4bfceceb6deb4df22f88f.

--- src/components/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/speakers">Speakers</router-link> |
-    <router-link to="/agenda">Agenda</router-link> |
-    <router-link to="/equipo">Equipo</router-link> |
-    <router-link to="/patrocinadores">Patrocinadores</router-link>
-  </div>
-</template>
--- src/router/index.js
+++ src/router/index.js
@@ -11,24 +11,12 @@ const routes = [
     component: Home,
   },
   {
-    path: '/speakers',
-    name: 'speakers',
-    component: () => import('../views/Speakers.vue'),
-  },
-  {
-    path: '/agenda',
-    name: 'agenda',
-    component: () => import('../views/Schedule.vue'),
-  },
-  {
-    path: '/equipo',
-    name: 'equipo',
-    component: () => import('../views/Team.vue'),
-  },
-  {
-    path: '/patrocinadores',
-    name: 'patrocinadores',
-    component: () => import('../views/Sponsors.vue'),
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
